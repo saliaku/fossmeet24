dfa196c43092d9fac6a7b967e5d3c729cdb7eccd
ipranavprashant <ipranavprashant@gmail.com>
2024-01-13 16:50:37 +0530

    footer included

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import ViewAllBtn from '@/components/common/ViewAllBtn'
+// import ViewAllBtn from '@/components/common/ViewAllBtn'
+import Footer from '@/components/footer/Footer'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Hello World!</h1>
-      <ViewAllBtn title={'View all Workshops'} link={'/'} />
+    <main>
+      <div className="flex flex-col items-center p-24">
+        <h1 className="text-4xl font-bold mb-8">Hello World!</h1>
+        {/* <ViewAllBtn title={'View all Workshops'} link={'/'} /> */}
+      </div>
+      <Footer />
     </main>
   )
 }
