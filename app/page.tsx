f8a62d46068bac2ca95d4df2c6af34924085acb1
Amal P Mathews <amalpmathews2003@gmail.com>
2024-01-13 17:47:06 +0530

    removed flex from main

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,23 @@
-// import ViewAllBtn from '@/components/common/ViewAllBtn'
 import Footer from '@/components/footer/Footer'
 
 import Workshopbox from '@/components/common/Workshopbox'
 
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24">
+    <main>
       <h1 className="text-4xl font-bold mb-8">Hello World!</h1>
-      <ViewAllBtn title={'View all Workshops'} link={'/'} />
 
-      <Workshopbox 
-       title={'FFmpeg Workshop'}
-       image={'/r1.png'}
-       date={'27 Jan'} 
-       time={'3hrs'} 
-       description={'Kerala Rustaceans is a group that aims to create a community of Rust enthusiasts who can learn, collaborate and share knowledge on projects related to Rust. The group also aims to promote the use of Rust in the state and to attract more developers to the language. They usually organise events, meetups, and hackathons to discuss and learn about the latest developments in Rust'} 
-       organizer={'CSEA'} 
-       organizerLogo={'/csea-small.png'}
-       />
-       
+      <Workshopbox
+        title={'FFmpeg Workshop'}
+        image={'/r1.png'}
+        date={'27 Jan'}
+        time={'3hrs'}
+        description={
+          'Kerala Rustaceans is a group that aims to create a community of Rust enthusiasts who can learn, collaborate and share knowledge on projects related to Rust. The group also aims to promote the use of Rust in the state and to attract more developers to the language. They usually organise events, meetups, and hackathons to discuss and learn about the latest developments in Rust'
+        }
+        organizer={'CSEA'}
+        organizerLogo={'/csea-small.png'}
+      />
 
       <Footer />
     </main>
